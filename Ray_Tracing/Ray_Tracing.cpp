06147031d69757a82adb0de632f3ae596f22aed7
Stagias <107843940+Stagias@users.noreply.github.com>
2024-03-01 02:12:11 +0100

    Initial commit

---
+++ Ray_Tracing/Ray_Tracing.cpp
@@ -0,0 +1,26 @@
+// Ray_Tracing.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include "rtweekend.h"
+
+#include "camera.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
+int main() {
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+
+    cam.render(world);
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
